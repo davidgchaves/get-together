909803c11bfb6f3e555781ec6749c72e5724cae4
David G Chaves <davidgchaves@gmail.com>
2015-02-20 20:30:04 +0100

    GREEN: ...handling a single string with no size, adds a task'
    'CreatesProject task string parsing when...

--- app/actions/creates_project.rb
+++ app/actions/creates_project.rb
@@ -1,16 +1,19 @@
 class CreatesProject
   attr_accessor :project
-  attr_reader :name
+  attr_reader :name, :task_string
 
   def initialize(name: "", task_string: "")
     @name = name
+    @task_string = task_string
   end
 
   def build
     self.project = Project.new name: name
+    project.tasks = convert_string_to_tasks
+    project
   end
 
   def convert_string_to_tasks
-    []
+    task_string.blank? ? [] : [Task.new]
   end
 end
