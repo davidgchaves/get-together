964d74dd8428ce02e62a3bc24997404735750616
David G Chaves <davidgchaves@gmail.com>
2015-02-20 17:09:07 +0100

    Add project spec described in a Given/When/Then commentary

---
+++ spec/features/add_project_spec.rb
@@ -0,0 +1,6 @@
+
+# Given: We're starting with empty data, so no setup
+# When:  Filling out a form with project data and submitting
+# Then:  Verifying that the new project shows up on our list of projects
+#        with the entered tasks attached
+
