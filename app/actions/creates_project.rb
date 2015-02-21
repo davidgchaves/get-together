f2062ca21ec92d8649951d890c10aeab586eb5a8
David G Chaves <davidgchaves@gmail.com>
2015-02-21 11:22:28 +0100

    Refactor convert_string_to_tasks

--- app/actions/creates_project.rb
+++ app/actions/creates_project.rb
@@ -1,5 +1,6 @@
 class CreatesProject
   DEFAULT_SIZE = 1
+
   attr_accessor :project
   attr_reader :name, :task_string
 
@@ -16,9 +17,13 @@ class CreatesProject
 
   def convert_string_to_tasks
     task_string.split(/\n/).map do |single_task_string|
-      title, size = single_task_string.split(/:/)
-      if size.nil? then size = DEFAULT_SIZE end
-      Task.new(title: title, size: size)
+      Task.new(parse_title_and_size_from single_task_string)
     end
   end
+
+  private
+    def parse_title_and_size_from(string)
+      title, size = string.split(/:/)
+      { title: title, size: size || DEFAULT_SIZE }
+    end
 end
