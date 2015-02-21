a662b181843234e2e31c56e4b8e9f791762fc1e4
David G Chaves <davidgchaves@gmail.com>
2015-02-21 13:43:27 +0100

    RED: 'A projects controller POST create commands CreateProject...
    ...to create the project'

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -12,4 +12,12 @@ describe ProjectsController, type: :controller do
       expect(response).to render_template :new
     end
   end
+
+  describe "POST create" do
+    before(:example) { post :create, project: { name: "Runaway", tasks: "Start something:2" } }
+
+    it "commands CreateProject to create the project" do
+      expect(assigns(:action).project.name).to eq "Runaway"
+    end
+  end
 end
