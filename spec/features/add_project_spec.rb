62cc421a93b83fa87c267736d4a3f7e01544253a
David G Chaves <davidgchaves@gmail.com>
2015-02-24 17:13:32 +0100

    Change describe/it for feature/scenario in acceptance test

--- spec/features/add_project_spec.rb
+++ spec/features/add_project_spec.rb
@@ -5,8 +5,8 @@ require "rails_helper"
 # Then:  Verifying that the new project shows up on our list of projects
 #        with the entered tasks attached
 
-describe "adding projects" do
-  it "allows a user to create a project with tasks" do
+feature "Adding projects" do
+  scenario "allows a user to create a project with tasks" do
     visit new_project_path
     fill_in "Name", with: "Project Runway"
     fill_in "Tasks", with: "Task 1:3\nTask 2:5" # 2 tasks sizes 3 and 5
