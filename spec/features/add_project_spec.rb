d490e36bbdc3f6c626299aeabf8457c6ffc0da3c
David G Chaves <davidgchaves@gmail.com>
2015-02-24 17:39:49 +0100

    Tighten 'Adding projects allows a user to create a project with tasks'

--- spec/features/add_project_spec.rb
+++ spec/features/add_project_spec.rb
@@ -9,11 +9,15 @@ feature "Adding projects" do
   scenario "allows a user to create a project with tasks" do
     visit new_project_path
     fill_in "Name", with: "Project Runway"
-    fill_in "Tasks", with: "Task 1:3\nTask 2:5" # 2 tasks sizes 3 and 5
+    fill_in "Tasks", with: "Task 1:3\nTask 2:5" # 2 tasks - sizes 3 and 5
     click_on "Create Project"
 
     visit projects_path
-    expect(page).to have_content "Project Runway"
-    expect(page).to have_content "8" # total size is 8 (=3+5)
+
+    project = Project.find_by name: "Project Runway"
+    within "#project_#{project.id}" do
+      expect(page).to have_selector ".name", text: "Project Runway"
+      expect(page).to have_selector ".total-size", text: "8" # total size is 8 (=3+5)
+    end
   end
 end
